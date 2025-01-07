1bb179e94379d356acfa9f6036ef172ebe990230
Biruk Lemma <biruklemmadebela@gmail.com>
2025-01-07 19:07:49 +0300

    api test

--- config/auth.php
+++ config/auth.php
@@ -40,12 +40,14 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
             'hash' => false,
         ],
+        
+
     ],
 
     /*

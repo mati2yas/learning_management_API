46bec34916900f64bb6f2b7726e5e96184ead5e9
Biruk Lemma <biruklemmadebela@gmail.com>
2025-03-08 01:26:14 +0300

    Refactor: Update create_paid_courses_table migration and ExamSeeder
    - Update create_paid_courses_table migration to fix a typo in a comment.
    - Refactor ExamSeeder to handle cases where `exam_course_id` is not needed.

--- database/seeders/ExamSeeder.php
+++ database/seeders/ExamSeeder.php
@@ -1,13 +1,11 @@
-<?php
+<?php 
 
 namespace Database\Seeders;
 
-use App\Http\Resources\Api\ExamCourseTypeResource;
 use App\Models\Exam;
-use App\Models\ExamCourse;
 use App\Models\ExamType;
 use App\Models\ExamYear;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ExamCourse;
 use Illuminate\Database\Seeder;
 
 class ExamSeeder extends Seeder
@@ -17,33 +15,34 @@ class ExamSeeder extends Seeder
      */
     public function run(): void
     {
-
         Exam::query()->delete();
-        if(Exam::count() === 0) {
-           $examTypes = ExamType::all();
-           $examYears = ExamYear::all();
-           $examCourses = ExamCourse::all();
+
+        // List of exam types that don't need `exam_course_id`
+        $noExamGradeTypes = ['NGAT', 'SAT', 'UAT', 'EXIT', 'EXAM'];
+
+        if (Exam::count() === 0) {
+            $examTypes = ExamType::all();
+            $examYears = ExamYear::all();
 
             foreach ($examTypes as $examType) {
-                foreach ($examCourses as $examCourse) {
-                    foreach ($examYears as $examYear) {
+                foreach ($examYears as $examYear) {
+                    // Generate base prices
+                    $priceOneMonth = rand(50, 200);
+                    $priceThreeMonth = $priceOneMonth * 2.5;
+                    $priceSixMonth = $priceOneMonth * 4.5;
+                    $priceOneYear = $priceOneMonth * 8;
 
-                        // Generate base prices
-                        $priceOneMonth = rand(50, 200);
-                        $priceThreeMonth = $priceOneMonth * 2.5;
-                        $priceSixMonth = $priceOneMonth * 4.5;
-                        $priceOneYear = $priceOneMonth * 8;
+                    // Generate discount prices (on sale)
+                    $onSaleOneMonth = round($priceOneMonth * (rand(75, 95) / 100), 2);
+                    $onSaleThreeMonth = round($priceThreeMonth * (rand(75, 95) / 100), 2);
+                    $onSaleSixMonth = round($priceSixMonth * (rand(75, 95) / 100), 2);
+                    $onSaleOneYear = round($priceOneYear * (rand(75, 95) / 100), 2);
 
-                        // Generate discount prices (on sale) ensuring they are lower than the original price
-                        $onSaleOneMonth = round($priceOneMonth * (rand(75, 95) / 100), 2);
-                        $onSaleThreeMonth = round($priceThreeMonth * (rand(75, 95) / 100), 2);
-                        $onSaleSixMonth = round($priceSixMonth * (rand(75, 95) / 100), 2);
-                        $onSaleOneYear = round($priceOneYear * (rand(75, 95) / 100), 2);
-
+                    // Handle cases where `exam_course_id` is not needed
+                    if (in_array($examType->name, $noExamGradeTypes)) {
                         Exam::factory()->create([
                             'exam_type_id' => $examType->id,
                             'exam_year_id' => $examYear->id,
-                            'exam_course_id' => $examCourse->id,
                             'price_one_month' => $priceOneMonth,
                             'price_three_month' => $priceThreeMonth,
                             'price_six_month' => $priceSixMonth,
@@ -52,12 +51,28 @@ class ExamSeeder extends Seeder
                             'on_sale_three_month' => $onSaleThreeMonth,
                             'on_sale_six_month' => $onSaleSixMonth,
                             'on_sale_one_year' => $onSaleOneYear,
-                            // 'price_one_month' => $exam
                         ]);
+                    } else {
+                        // For other types, use `exam_course_id`
+                        $examCourses = ExamCourse::all();
+                        foreach ($examCourses as $examCourse) {
+                            Exam::factory()->create([
+                                'exam_type_id' => $examType->id,
+                                'exam_year_id' => $examYear->id,
+                                'exam_course_id' => $examCourse->id,
+                                'price_one_month' => $priceOneMonth,
+                                'price_three_month' => $priceThreeMonth,
+                                'price_six_month' => $priceSixMonth,
+                                'price_one_year' => $priceOneYear,
+                                'on_sale_one_month' => $onSaleOneMonth,
+                                'on_sale_three_month' => $onSaleThreeMonth,
+                                'on_sale_six_month' => $onSaleSixMonth,
+                                'on_sale_one_year' => $onSaleOneYear,
+                            ]);
+                        }
                     }
                 }
             }
-
         }
     }
 }
